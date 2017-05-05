f574662c0e58e0c963203fe9636f8ec73630610a
OHMAE Ryosuke <ryo@mm2d.net>
2017-05-06 00:50:08 +0900

    If default app is installed, use it instead of chrome

--- mobile/src/main/java/net/mm2d/dmsexplorer/domain/model/OpenUriCustomTabsModel.java
+++ mobile/src/main/java/net/mm2d/dmsexplorer/domain/model/OpenUriCustomTabsModel.java
@@ -10,6 +10,9 @@ package net.mm2d.dmsexplorer.domain.model;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -19,6 +22,8 @@ import android.webkit.URLUtil;
 
 import net.mm2d.android.util.LaunchUtils;
 import net.mm2d.dmsexplorer.R;
+
+import java.util.List;
 
 /**
  * @author <a href="mailto:ryo@mm2d.net">大前良介 (OHMAE Ryosuke)</a>
@@ -43,7 +48,8 @@ public class OpenUriCustomTabsModel implements OpenUriModel {
 
     @Override
     public void openUri(@NonNull final Context context, @NonNull final String uri) {
-        if (!mUseCustomTabs || !URLUtil.isNetworkUrl(uri)) {
+        if (!mUseCustomTabs || !URLUtil.isNetworkUrl(uri)
+                || hasDefaultAppOtherThanBrowser(context, uri)) {
             LaunchUtils.openUri(context, uri);
             return;
         }
@@ -78,5 +84,32 @@ public class OpenUriCustomTabsModel implements OpenUriModel {
         }
         final int color = mThemeModel.getToolbarColor((Activity) context);
         return color != 0 ? color : DEFAULT_TOOLBAR_COLOR;
+    }
+
+    private static boolean hasDefaultAppOtherThanBrowser(
+            @NonNull final Context context, @NonNull final String uri) {
+        final PackageManager pm = context.getPackageManager();
+        final List<ResolveInfo> browsers = pm.queryIntentActivities(
+                makeViewIntent("http://www.example.com/"), 0);
+        final Intent intent = makeViewIntent(uri);
+        final List<ResolveInfo> apps = pm.queryIntentActivities(intent, 0);
+        final ResolveInfo defaultApp = pm.resolveActivity(intent, 0);
+        return contains(defaultApp, apps) && !contains(defaultApp, browsers);
+    }
+
+    private static Intent makeViewIntent(@NonNull final String uri) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        return intent;
+    }
+
+    private static boolean contains(@NonNull final ResolveInfo target, @NonNull final List<ResolveInfo> list) {
+        final String packageName = target.activityInfo.packageName;
+        for (final ResolveInfo info : list) {
+            if (TextUtils.equals(packageName, info.activityInfo.packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
