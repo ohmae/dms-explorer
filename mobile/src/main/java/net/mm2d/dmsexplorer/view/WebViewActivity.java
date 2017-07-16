cabee997377c0c5ca9416c2a08f5439022117a5d
OHMAE Ryosuke <ryo@mm2d.net>
2017-07-16 19:46:16 +0900

    add progress bar

--- mobile/src/main/java/net/mm2d/dmsexplorer/view/WebViewActivity.java
+++ mobile/src/main/java/net/mm2d/dmsexplorer/view/WebViewActivity.java
@@ -10,10 +10,12 @@ package net.mm2d.dmsexplorer.view;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -62,7 +64,27 @@ public class WebViewActivity extends BaseActivity {
         final WebView webView = binding.webView;
         webView.getSettings().setSupportZoom(false);
         webView.getSettings().setDisplayZoomControls(false);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(final WebView view, final int newProgress) {
+                binding.progress.setProgress(newProgress);
+                if (newProgress == 100) {
+                    binding.progress.setVisibility(View.GONE);
+                }
+            }
+        });
         webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(final WebView view, final String url, final Bitmap favicon) {
+                binding.progress.setProgress(0);
+                binding.progress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(final WebView view, final String url) {
+                binding.progress.setVisibility(View.GONE);
+            }
+
             @SuppressWarnings("deprecation")
             @Override
             public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
@@ -70,7 +92,6 @@ public class WebViewActivity extends BaseActivity {
                 return true;
             }
         });
-        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl(intent.getStringExtra(KEY_URL));
     }
 }
