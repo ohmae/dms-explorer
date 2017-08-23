c5b42fe08baf6e104b35e8c43649bd939a4c5abc
OHMAE Ryosuke <ryo@mm2d.net>
2017-08-23 21:48:45 +0900

    fix typo

--- mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/helper/MovieActivityPipHelperOreo.java
+++ mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/helper/MovieActivityPipHelperOreo.java
@@ -93,7 +93,7 @@ class MovieActivityPipHelperOreo implements MovieActivityPipHelper {
     @Override
     public void unregister() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            unregister();
+            mActivity.unregisterReceiver(mControlReceiver);
         }
     }
 
