1332858ebe9df6d893159c9608fc34a1e3db92ab
OHMAE Ryosuke <ryo@mm2d.net>
2017-08-23 20:32:12 +0900

    modify picture in picture.

--- mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/MovieActivityModel.java
+++ mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/MovieActivityModel.java
@@ -7,18 +7,25 @@
 
 package net.mm2d.dmsexplorer.viewmodel;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.PictureInPictureParams;
+import android.app.RemoteAction;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.Icon;
 import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Rational;
+import android.view.View;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -35,8 +42,14 @@ import net.mm2d.dmsexplorer.domain.model.PlaybackTargetModel;
 import net.mm2d.dmsexplorer.domain.model.PlayerModel;
 import net.mm2d.dmsexplorer.settings.RepeatMode;
 import net.mm2d.dmsexplorer.settings.Settings;
+import net.mm2d.dmsexplorer.view.MovieActivity;
 import net.mm2d.dmsexplorer.viewmodel.ControlPanelModel.OnCompletionListener;
 import net.mm2d.dmsexplorer.viewmodel.ControlPanelModel.SkipControlListener;
+import net.mm2d.util.Log;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author <a href="mailto:ryo@mm2d.net">大前良介 (OHMAE Ryosuke)</a>
@@ -79,8 +92,7 @@ public class MovieActivityModel extends BaseObservable
     private final MediaServerModel mServerModel;
     @NonNull
     private final Settings mSettings;
-    @Nullable
-    private PictureInPictureParams mPictureInPictureParams;
+    private PlayerModel mPlayerModel;
 
     public MovieActivityModel(
             @NonNull final Activity activity,
@@ -105,12 +117,12 @@ public class MovieActivityModel extends BaseObservable
         if (targetModel == null) {
             throw new IllegalStateException();
         }
-        final PlayerModel playerModel = new MoviePlayerModel(mVideoView);
-        mControlPanelModel = new ControlPanelModel(mActivity, playerModel);
+        mPlayerModel = new MoviePlayerModel(mVideoView);
+        mControlPanelModel = new ControlPanelModel(mActivity, mPlayerModel);
         mControlPanelModel.setRepeatMode(mRepeatMode);
         mControlPanelModel.setOnCompletionListener(this);
         mControlPanelModel.setSkipControlListener(this);
-        playerModel.setUri(targetModel.getUri(), null);
+        mPlayerModel.setUri(targetModel.getUri(), null);
         mTitle = AribUtils.toDisplayableString(targetModel.getTitle());
 
         notifyPropertyChanged(BR.title);
@@ -154,13 +166,83 @@ public class MovieActivityModel extends BaseObservable
 
     public void onClickPictureInPicture() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (mPictureInPictureParams == null) {
-                mPictureInPictureParams = new PictureInPictureParams.Builder()
-                        .setAspectRatio(new Rational(16, 9))
-                        .build();
+            final PictureInPictureParams.Builder builder = new PictureInPictureParams.Builder()
+                    .setActions(makeActions());
+            final Rect rect = makeViewRect(mVideoView);
+            Log.e(rect.toString());
+            if (rect.width() > 0 && rect.height() > 0) {
+                builder.setAspectRatio(new Rational(rect.width(), rect.height()))
+                        .setSourceRectHint(rect);
             }
-            mActivity.enterPictureInPictureMode(mPictureInPictureParams);
+            try {
+                mActivity.enterPictureInPictureMode(builder.build());
+            } catch (final Exception e) {
+                Log.w(e);
+            }
         }
+    }
+
+    @NonNull
+    private Rect makeViewRect(final View v) {
+        final Rect rect = new Rect();
+        v.getGlobalVisibleRect(rect);
+        return rect;
+    }
+
+    @TargetApi(VERSION_CODES.O)
+    private List<RemoteAction> makeActions() {
+        final int max = mActivity.getMaxNumPictureInPictureActions();
+        if (max <= 0) {
+            return Collections.emptyList();
+        }
+        if (max >= 3) {
+            return Arrays.asList(
+                    makePreviousAction(),
+                    makePlayAction(),
+                    makeNextAction()
+            );
+        }
+        return Arrays.asList(makePlayAction());
+    }
+
+    @TargetApi(VERSION_CODES.O)
+    @NonNull
+    private RemoteAction makePlayAction() {
+        return new RemoteAction(
+                makeIcon(R.drawable.ic_play),
+                getString(R.string.action_play_title),
+                getString(R.string.action_play_description),
+                MovieActivity.makePlayPendingIntent(mActivity));
+    }
+
+    @TargetApi(VERSION_CODES.O)
+    @NonNull
+    private RemoteAction makeNextAction() {
+        return new RemoteAction(
+                makeIcon(R.drawable.ic_skip_next),
+                getString(R.string.action_next_title),
+                getString(R.string.action_next_description),
+                MovieActivity.makeNextPendingIntent(mActivity));
+    }
+
+    @TargetApi(VERSION_CODES.O)
+    @NonNull
+    private RemoteAction makePreviousAction() {
+        return new RemoteAction(
+                makeIcon(R.drawable.ic_skip_previous),
+                getString(R.string.action_previous_title),
+                getString(R.string.action_previous_description),
+                MovieActivity.makePreviousPendingIntent(mActivity));
+    }
+
+    @NonNull
+    private String getString(@StringRes final int resId) {
+        return mActivity.getResources().getText(resId, "").toString();
+    }
+
+    @TargetApi(VERSION_CODES.M)
+    private Icon makeIcon(@DrawableRes final int resId) {
+        return Icon.createWithResource(mActivity, resId);
     }
 
     private void showRepeatToast() {
