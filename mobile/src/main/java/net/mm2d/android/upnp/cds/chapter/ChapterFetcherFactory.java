336cde6d6316af1da7f38cdb7fbc4a3f32903b18
OHMAE Ryosuke <ryo@mm2d.net>
2017-09-06 23:04:46 +0900

    rename

--- mobile/src/main/java/net/mm2d/android/upnp/cds/chapter/ChapterFetcherFactory.java
+++ mobile/src/main/java/net/mm2d/android/upnp/cds/chapter/ChapterFetcherFactory.java
@@ -19,15 +19,15 @@ import io.reactivex.Single;
  * @author <a href="mailto:ryo@mm2d.net">大前良介 (OHMAE Ryosuke)</a>
  */
 public class ChapterFetcherFactory {
-    private static final FetcherFactory[] sFetchers = new FetcherFactory[]{
+    private static final FetcherFactory[] sFactories = new FetcherFactory[]{
             new SonyFetcherFactory(),
             new PanasonicFetcherFactory(),
     };
 
     @NonNull
     public static Single<List<Integer>> create(@NonNull final CdsObject object) {
-        for (final FetcherFactory fetcher : sFetchers) {
-            final Single<List<Integer>> single = fetcher.create(object);
+        for (final FetcherFactory factory : sFactories) {
+            final Single<List<Integer>> single = factory.create(object);
             if (single != null) {
                 return single;
             }
