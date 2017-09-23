e3060a318e6aaf6d5794a67b9312a0242f60eb3d
OHMAE Ryosuke <ryo@mm2d.net>
2017-09-23 14:02:34 +0900

    Make app work on also an ethernet connection. #42

--- mobile/src/main/java/net/mm2d/android/net/AndroidLan.java
+++ mobile/src/main/java/net/mm2d/android/net/AndroidLan.java
@@ -10,17 +10,16 @@ package net.mm2d.android.net;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import net.mm2d.util.NetworkUtils;
 
-import java.net.InetAddress;
+import java.net.Inet4Address;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
-import java.net.UnknownHostException;
+import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -58,62 +57,37 @@ class AndroidLan extends Lan {
     @NonNull
     @Override
     public Collection<NetworkInterface> getAvailableInterfaces() {
-        if (!hasWifiConnection()) {
+        if (!hasAvailableInterface()) {
             return Collections.emptyList();
         }
-        final InetAddress address = getWifiInetAddress();
-        if (address == null) {
-            return Collections.emptyList();
-        }
-        final List<NetworkInterface> netIfList = NetworkUtils.getNetworkInterfaceList();
-        for (final NetworkInterface netIf : netIfList) {
-            if (hasAddress(netIf, address)) {
-                return Collections.singletonList(netIf);
+        final List<NetworkInterface> result = new ArrayList<>();
+        for (final NetworkInterface netIf : NetworkUtils.getNetworkInterfaceList()) {
+            if (isUsableInterface(netIf)) {
+                result.add(netIf);
             }
         }
-        return Collections.emptyList();
+        return result;
     }
 
-    private static boolean hasAddress(
-            @NonNull final NetworkInterface netIf,
-            @NonNull final InetAddress targetAddress) {
-        final List<InterfaceAddress> addressList = netIf.getInterfaceAddresses();
-        for (final InterfaceAddress address : addressList) {
-            if (address.getAddress().equals(targetAddress)) {
-                return true;
+    private boolean isUsableInterface(@NonNull final NetworkInterface netIf) {
+        try {
+            if (!netIf.isUp()) {
+                return false;
             }
+            if (!netIf.supportsMulticast()) {
+                return false;
+            }
+            if (netIf.isLoopback()) {
+                return false;
+            }
+            final List<InterfaceAddress> addresses = netIf.getInterfaceAddresses();
+            for (final InterfaceAddress address : addresses) {
+                if (address.getAddress() instanceof Inet4Address) {
+                    return true;
+                }
+            }
+        } catch (final SocketException ignored) {
         }
         return false;
-    }
-
-    private boolean hasWifiConnection() {
-        final NetworkInfo info = mConnectivityManager.getActiveNetworkInfo();
-        return info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI;
-    }
-
-    @Nullable
-    private InetAddress getWifiInetAddress() {
-        final WifiInfo wifiInfo = mWifiManager.getConnectionInfo();
-        if (wifiInfo == null) {
-            return null;
-        }
-        try {
-            return InetAddress.getByAddress(intToByteArray(wifiInfo.getIpAddress()));
-        } catch (final UnknownHostException ignored) {
-        }
-        return null;
-    }
-
-    @NonNull
-    private byte[] intToByteArray(int ip) {
-        final byte[] array = new byte[4];
-        array[0] = (byte) (ip & 0xff);
-        ip >>= 8;
-        array[1] = (byte) (ip & 0xff);
-        ip >>= 8;
-        array[2] = (byte) (ip & 0xff);
-        ip >>= 8;
-        array[3] = (byte) (ip & 0xff);
-        return array;
     }
 }
