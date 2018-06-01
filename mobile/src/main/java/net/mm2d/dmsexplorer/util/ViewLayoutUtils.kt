7117a21257bcaee8f70fe1c044cc7fcc59351bd1
OHMAE Ryosuke <ryo@mm2d.net>
2018-06-02 00:00:40 +0900

    feature: convert to kotlin

---
+++ mobile/src/main/java/net/mm2d/dmsexplorer/util/ViewLayoutUtils.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.dmsexplorer.util
+
+import android.support.annotation.Px
+import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
+
+/**
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
+ */
+object ViewLayoutUtils {
+    @JvmStatic
+    fun setLayoutMarginRight(view: View, @Px margin: Int) {
+        val params = view.layoutParams as? MarginLayoutParams ?: return
+        params.rightMargin = margin
+        view.layoutParams = params
+    }
+
+    @JvmStatic
+    fun setPaddingBottom(view: View, @Px padding: Int) {
+        view.updatePadding(bottom = padding)
+    }
+
+    private fun View.updatePadding(
+            @Px left: Int = paddingLeft,
+            @Px top: Int = paddingTop,
+            @Px right: Int = paddingRight,
+            @Px bottom: Int = paddingBottom) {
+        setPadding(left, top, right, bottom)
+    }
+}
