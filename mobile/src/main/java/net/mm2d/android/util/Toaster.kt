ba8482a6e9878d830c7eb1bfc960891ab15d1977
OHMAE Ryosuke <ryo@mm2d.net>
2019-02-08 23:49:04 +0900

    :recycle: convert java to kotlin

---
+++ mobile/src/main/java/net/mm2d/android/util/Toaster.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.android.util
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+/**
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
+ */
+object Toaster {
+    @JvmStatic
+    fun show(context: Context?, @StringRes resId: Int): Toast? {
+        return context?.let {
+            Toast.makeText(it, resId, Toast.LENGTH_LONG).apply { show() }
+        }
+    }
+}
