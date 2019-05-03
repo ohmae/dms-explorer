5a12b6a5aba8543af7d1d673521bdb4634a6c852
OHMAE Ryosuke <ryo@mm2d.net>
2019-05-04 00:25:26 +0900

    :recycle: convert java to kotlin

--- mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/RendererItemModel.kt
+++ mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/RendererItemModel.kt
@@ -12,7 +12,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.DrawableCompat
-import net.mm2d.android.upnp.cds.MediaServer
+import net.mm2d.android.upnp.avt.MediaRenderer
 import net.mm2d.android.util.DrawableUtils
 import net.mm2d.android.util.toDisplayableString
 import net.mm2d.dmsexplorer.R
@@ -21,10 +21,9 @@ import net.mm2d.dmsexplorer.settings.Settings
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
-class ServerItemModel(
+class RendererItemModel(
     context: Context,
-    server: MediaServer,
-    val selected: Boolean
+    renderer: MediaRenderer
 ) {
     var accentBackground: Drawable?
     var accentText: String?
@@ -33,37 +32,37 @@ class ServerItemModel(
     val description: String
 
     init {
-        val name = server.friendlyName
+        val name = renderer.friendlyName
         title = name
-        description = makeDescription(server)
-        val params = Settings.get().themeParams
-        val binary = server.getIcon()?.binary
+        description = makeDescription(renderer)
+        val binary = renderer.getIcon()?.binary
         if (binary == null) {
             accentIcon = null
             accentText = if (name.isEmpty()) "" else name.substring(0, 1).toDisplayableString()
+            val generator = Settings.get()
+                .themeParams
+                .themeColorGenerator
             accentBackground = DrawableUtils.get(context, R.drawable.ic_circle)?.also {
                 it.mutate()
-                DrawableCompat.setTint(it, params.themeColorGenerator.getIconColor(name))
+                DrawableCompat.setTint(it, generator.getIconColor(name))
             }
-            params.serverColorExtractor.invokeAsync(server, null)
         } else {
             accentIcon = BitmapFactory.decodeByteArray(binary, 0, binary.size)
             accentText = null
             accentBackground = null
-            params.serverColorExtractor.invokeAsync(server, accentIcon)
         }
     }
 
-    private fun makeDescription(server: MediaServer): String {
+    private fun makeDescription(renderer: MediaRenderer): String {
         val sb = StringBuilder()
-        val manufacture = server.manufacture
+        val manufacture = renderer.manufacture
         if (!manufacture.isNullOrEmpty()) {
             sb.append(manufacture)
             sb.append('\n')
         }
         sb.append("IP: ")
-        sb.append(server.ipAddress)
-        val serial = server.serialNumber
+        sb.append(renderer.ipAddress)
+        val serial = renderer.serialNumber
         if (!serial.isNullOrEmpty()) {
             sb.append("  Serial: ")
             sb.append(serial)
