04e94d5d4a9ac347839a7613c95c19d8653c1596
OHMAE Ryosuke <ryo@mm2d.net>
2019-05-03 19:51:06 +0900

    :recycle: convert java to kotlin

---
+++ mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/adapter/RecyclerViewBindingAdapter.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.dmsexplorer.viewmodel.adapter
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
+ */
+object RecyclerViewBindingAdapter {
+    // 選択項目を中央に表示させる処理
+    // FIXME: DataBindingを使ったことで返って複雑化してしまっている
+    @BindingAdapter("scrollPosition")
+    @JvmStatic
+    fun setScrollPosition(
+        view: RecyclerView,
+        position: Int
+    ) {
+        if (position < 0) {
+            return
+        }
+        view.scrollToPosition(position)
+        view.post { scrollToCenter(view, position) }
+    }
+
+    private fun scrollToCenter(
+        view: RecyclerView,
+        position: Int
+    ) {
+        val child = findPositionView(view, position) ?: return
+        val dy = (child.top - (view.height - child.height) / 2f).toInt()
+        view.smoothScrollBy(0, dy)
+    }
+
+    private fun findPositionView(
+        view: RecyclerView,
+        position: Int
+    ): View? {
+        val child = view.getChildAt(0) ?: return null
+        val top = view.getChildAdapterPosition(child)
+        return if (top < 0) null else view.getChildAt(position - top)
+    }
+}
---
+++ mobile/src/main/java/net/mm2d/dmsexplorer/viewmodel/adapter/TextViewBindingAdapter.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.dmsexplorer.viewmodel.adapter
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import net.mm2d.dmsexplorer.util.AttrUtils
+
+/**
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
+ */
+object TextViewBindingAdapter {
+    @BindingAdapter("underline")
+    @JvmStatic
+    fun setUnderlineFlag(
+        view: TextView,
+        underline: Boolean
+    ) {
+        view.paintFlags = if (underline) {
+            view.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        } else {
+            view.paintFlags and Paint.UNDERLINE_TEXT_FLAG.inv()
+        }
+    }
+
+    @BindingAdapter("textColorAttr")
+    @JvmStatic
+    fun setTextColorAttr(
+        view: TextView,
+        attr: Int
+    ) {
+        view.setTextColor(AttrUtils.resolveColor(view.context, attr, Color.BLACK))
+    }
+}
