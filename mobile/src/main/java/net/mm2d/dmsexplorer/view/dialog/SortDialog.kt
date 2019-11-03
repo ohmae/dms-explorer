aa3e6bfc177096f719571edc1046b4735656ed1f
OHMAE Ryosuke <ryo@mm2d.net>
2019-11-03 22:16:56 +0900

    ✨use Spinner

--- mobile/src/main/java/net/mm2d/dmsexplorer/view/dialog/SortDialog.kt
+++ mobile/src/main/java/net/mm2d/dmsexplorer/view/dialog/SortDialog.kt
@@ -9,19 +9,24 @@ package net.mm2d.dmsexplorer.view.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
+import kotlinx.android.synthetic.main.dialog_sort.view.*
 import net.mm2d.dmsexplorer.R
 import net.mm2d.dmsexplorer.Repository
 import net.mm2d.dmsexplorer.settings.Settings
 import net.mm2d.dmsexplorer.settings.SortKey
-import net.mm2d.dmsexplorer.settings.SortKey.*
 
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
@@ -32,24 +37,38 @@ class SortDialog : DialogFragment() {
         val settings = Settings.get()
         val parent = activity.window.decorView as ViewGroup
         val view = activity.layoutInflater.inflate(R.layout.dialog_sort, parent, false)
-        val sortOrder: SwitchCompat = view.findViewById(R.id.sort_order)
+        val sortOrder: SwitchCompat = view.sort_order
         sortOrder.setOnCheckedChangeListener { _, isChecked ->
             val id =
                 if (isChecked) R.string.dialog_sort_order_ascending else R.string.dialog_sort_order_descending
             sortOrder.setText(id)
         }
         sortOrder.isChecked = settings.isAscendingSortOrder
-        val sortKey: RadioGroup = view.findViewById(R.id.sort_key)
-        sortKey.setOnCheckedChangeListener { _, checkedId ->
-            sortOrder.isEnabled = checkedId != R.id.sort_key_none
+
+        val sortKey: Spinner = view.sort_key
+        sortKey.adapter = ArrayAdapter<String>(
+            activity,
+            android.R.layout.simple_list_item_1,
+            list.map { getString(it.stringRes) })
+        sortKey.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>) = Unit
+
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                sortOrder.isEnabled = position != list.indexOfFirst { it.sortKey == SortKey.NONE }
+            }
         }
-        sortKey.check(settings.sortKey.toId())
+        sortKey.setSelection(list.indexOfFirst { it.sortKey == settings.sortKey })
         parentFragment
         return AlertDialog.Builder(activity)
             .setTitle(R.string.dialog_title_sort)
             .setView(view)
             .setPositiveButton(R.string.ok) { _, _ ->
-                val key = sortKey.checkedRadioButtonId.toSortKey()
+                val key = list[sortKey.selectedItemPosition].sortKey
                 val isAscending = sortOrder.isChecked
                 if (key == settings.sortKey && isAscending == settings.isAscendingSortOrder) {
                     return@setPositiveButton
@@ -72,24 +91,23 @@ class SortDialog : DialogFragment() {
         }
     }
 
-    private fun SortKey.toId(): Int = when (this) {
-        NONE -> R.id.sort_key_none
-        NAME -> R.id.sort_key_name
-        DATE -> R.id.sort_key_date
-    }
-
-    private fun Int.toSortKey(): SortKey = when (this) {
-        R.id.sort_key_none -> NONE
-        R.id.sort_key_name -> NAME
-        R.id.sort_key_date -> DATE
-        else -> NONE
-    }
-
     interface OnUpdateSortSettings {
         fun onUpdateSortSettings()
     }
 
+    private class SpinnerItem(
+        val sortKey: SortKey,
+        @StringRes
+        val stringRes: Int
+    )
+
     companion object {
+        private val list = listOf(
+            SpinnerItem(SortKey.NONE, R.string.dialog_sort_key_none),
+            SpinnerItem(SortKey.NAME, R.string.dialog_sort_key_name),
+            SpinnerItem(SortKey.DATE, R.string.dialog_sort_key_date)
+        )
+
         private fun newInstance(): SortDialog = SortDialog()
 
         fun show(activity: FragmentActivity) {
