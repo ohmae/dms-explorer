7016ffbd5288881c33cf619e9434e6153398fd71
OHMAE Ryosuke <ryo@mm2d.net>
2019-11-03 21:19:31 +0900

    📖add copyright

--- mobile/src/main/java/net/mm2d/dmsexplorer/domain/entity/CdsContentEntity.kt
+++ mobile/src/main/java/net/mm2d/dmsexplorer/domain/entity/CdsContentEntity.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.dmsexplorer.domain.entity
 
 import android.net.Uri
--- mobile/src/main/java/net/mm2d/dmsexplorer/domain/entity/SortUtils.kt
+++ mobile/src/main/java/net/mm2d/dmsexplorer/domain/entity/SortUtils.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.dmsexplorer.domain.entity
 
 import java.text.Collator
