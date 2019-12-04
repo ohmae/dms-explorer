203aa423b2be3507a3978ac344077964f00430ba
OHMAE Ryosuke <ryo@mm2d.net>
2019-12-04 22:24:02 +0900

    💚fix ci test

---
+++ mobile/src/test/java/net/mm2d/dmsexplorer/TestApplication.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 大前良介 (OHMAE Ryosuke)
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.dmsexplorer
+
+/**
+ * Fake class for testing.
+ *
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
+ */
+class TestApplication: App()
