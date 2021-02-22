1fbc6927e05307e1254315cef0054065d75c3b40
OHMAE Ryosuke <ryo@mm2d.net>
2021-02-22 22:28:04 +0900

    💚remove jcenter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 buildscript {
     repositories {
         google()
-        mavenCentral()
-        jcenter()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
@@ -16,7 +15,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        jcenter()
     }
 }
 
