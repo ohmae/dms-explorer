a1bb3254812454b8ff0c2260fae0f10836b88c47
OHMAE Ryosuke <ryo@mm2d.net>
2021-05-09 13:58:17 +0900

    📖copyright comment

--- mobile/src/main/java/net/mm2d/dmsexplorer/util/UpdateChecker.kt
+++ mobile/src/main/java/net/mm2d/dmsexplorer/util/UpdateChecker.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.dmsexplorer.util
 
 import android.app.Activity
