54bc376221f77a3ed908f593167483bc09d45b37
OHMAE Ryosuke <ryo@mm2d.net>
2022-04-29 14:17:35 +0900

    🆙kotlin/gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.2")
-        classpath(kotlin("gradle-plugin", version = "1.6.10"))
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath(kotlin("gradle-plugin", version = "1.6.21"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
 
+        // for release
     }
 }
 
--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -3,10 +3,12 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-kapt")
+    kotlin("android")
+    kotlin("kapt")
     jacoco
     id("com.github.ben-manes.versions")
+
+    // for release
 }
 
 val applicationName = "DmsExplorer"
@@ -88,25 +90,27 @@ dependencies {
     implementation("com.google.android.play:core:1.10.3")
     implementation("com.google.android.play:core-ktx:1.8.1")
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
-    implementation("net.mm2d.mmupnp:mmupnp:3.1.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation("net.mm2d.mmupnp:mmupnp:3.1.6")
     implementation("net.mm2d.preference:preference:0.2.8")
     implementation("io.reactivex.rxjava2:rxjava:2.2.21")
     implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
     implementation("io.reactivex.rxjava2:rxkotlin:2.4.0")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk:1.12.3")
     testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("org.robolectric:robolectric:4.7.3")
+    testImplementation("org.robolectric:robolectric:4.8")
     testImplementation("androidx.test.ext:junit:1.1.3")
 
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.8.1")
-    debugImplementation("com.facebook.flipper:flipper:0.136.0")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
+    debugImplementation("com.facebook.flipper:flipper:0.144.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.3")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.136.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.136.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.144.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.144.0")
+
+    // for release
 }
 
 jacoco {
