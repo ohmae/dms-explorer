93a4fb861f92193d761292e0f987a7e6308e8887
OHMAE Ryosuke <ryo@mm2d.net>
2022-10-22 23:16:38 +0900

    🆙flipper

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.android.material:material:1.7.0")
     implementation("com.google.android.play:core:1.10.3")
     implementation("com.google.android.play:core-ktx:1.8.1")
     implementation(kotlin("reflect"))
@@ -102,10 +102,10 @@ dependencies {
     testImplementation("androidx.test.ext:junit:1.1.3")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
-    debugImplementation("com.facebook.flipper:flipper:0.170.0")
+    debugImplementation("com.facebook.flipper:flipper:0.171.1")
     debugImplementation("com.facebook.soloader:soloader:0.10.4")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.170.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.170.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.171.1")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.171.1")
 
     // for release
 }
