7cca529d09339778d06f42be936905f7660d8991
OHMAE Ryosuke <ryo@mm2d.net>
2023-09-02 13:42:04 +0900

    🐛revert AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.github.ben-manes.versions") version "0.47.0" apply false
 
     // for release
-}
-
-tasks.create("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
