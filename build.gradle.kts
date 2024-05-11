f35ca362a92438a56edcb54b3fbbeaf6aa92e6e6
OHMAE Ryosuke <ryo@mm2d.net>
2024-05-11 23:20:55 +0900

    🆙AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.github.ben-manes.versions") version "0.51.0" apply false
     id("org.jetbrains.kotlinx.kover") version "0.7.4" apply false
 
